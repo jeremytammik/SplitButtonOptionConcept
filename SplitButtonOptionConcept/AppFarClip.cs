15badd75f3ae04388f3b73bdeb54912f1a5806ea
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2016-09-06 12:35:04 +0200

    cleanup

--- SplitButtonOptionConcept/AppFarClip.cs
+++ SplitButtonOptionConcept/AppFarClip.cs
@@ -14,15 +14,23 @@ namespace SplitButtonOptionConcept
 {
   class AppFarClip : IExternalApplication
   {
-    static string _path = typeof( Application ).Assembly.Location;
+    //static string _path = typeof( Application ).Assembly.Location;
+
     SplitButton sbFarClip;
+
+    /// <summary>
     /// this external application class instance.
+    /// </summary>
     internal static AppFarClip _app = null;
+
+    /// <summary>
     /// Provide access to this class instance.
+    /// </summary>
     public static AppFarClip Instance
     {
       get { return _app; }
     }
+
     public Result OnStartup( UIControlledApplication a )
     {
       _app = this;
@@ -35,40 +43,67 @@ namespace SplitButtonOptionConcept
       return Result.Succeeded;
     }
 
-    public void AddFarClipUtil_Example_Ribbon( UIControlledApplication a )
+    public void AddFarClipUtil_Example_Ribbon( 
+      UIControlledApplication a )
     {
-      string ExecutingAssemblyPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
-      string ExecutingAssemblyName = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
+      System.Reflection.Assembly assembly = System.Reflection
+        .Assembly.GetExecutingAssembly();
+
+      string ass_path = assembly.Location;
+      string ass_name = assembly.GetName().Name;
+
       // create ribbon tab 
       String thisNewTabName = "SPLITB_OPT";
+
       try
       {
         a.CreateRibbonTab( thisNewTabName );
       }
       catch( Autodesk.Revit.Exceptions.ArgumentException )
       {
-        // Assume error generated is due to "WTA" already existing
+        // Assume error generated is due to tab already existing
       }
-      PushButtonData pbSecAdjust = new PushButtonData( "FarSideClip", "Far Clip", ExecutingAssemblyPath, ExecutingAssemblyName + ".SectionFarClipReset" );
-      PushButtonData pbSecAdjustOpt = new PushButtonData( "FarSideClipOpt", "Settings", ExecutingAssemblyPath, ExecutingAssemblyName + ".SectionFarClipResetOptions" );
 
-      pbSecAdjust.LargeImage = NewBitmapImage( System.Reflection.Assembly.GetExecutingAssembly(), "SplitButtonOptionConcept.FarClip.png" );
-      pbSecAdjustOpt.LargeImage = NewBitmapImage( System.Reflection.Assembly.GetExecutingAssembly(), "SplitButtonOptionConcept.FarClipSetting.png" );
+      PushButtonData pbSecAdjust = new PushButtonData( 
+        "FarSideClip", "Far Clip", ass_path, 
+        ass_name + ".SectionFarClipReset" );
+
+      PushButtonData pbSecAdjustOpt = new PushButtonData( 
+        "FarSideClipOpt", "Settings", ass_path, 
+        ass_name + ".SectionFarClipResetOptions" );
+
+      pbSecAdjust.LargeImage = NewBitmapImage( assembly, 
+        "SplitButtonOptionConcept.FarClip.png" );
+
+      pbSecAdjustOpt.LargeImage = NewBitmapImage( assembly,
+        "SplitButtonOptionConcept.FarClipSetting.png" );
 
       // add button tips (when data, must be defined prior to adding button.)
-      pbSecAdjust.ToolTip = "Reset a section's far clipping boundary to a close distance.";
-      pbSecAdjust.LongDescription = "Start this command and pick a section line. The far clipping distance will be reset to be close to the line.";
+
+      pbSecAdjust.ToolTip = "Reset a section's far clipping "
+        + "boundary to a close distance.";
+
+      pbSecAdjust.LongDescription = "Start this command and "
+        + "pick a section line. The far clipping distance "
+        + "will be reset to be close to the line.";
 
       //   Add new ribbon panel. 
+
       String thisNewPanelName = "Misc Utils";
-      RibbonPanel thisNewRibbonPanel = a.CreateRibbonPanel( thisNewTabName, thisNewPanelName );
+
+      RibbonPanel thisNewRibbonPanel = a.CreateRibbonPanel( 
+        thisNewTabName, thisNewPanelName );
 
       // add button to ribbon panel
-      SplitButtonData sbFarClipData = new SplitButtonData( "splitFarClip", "FarClip" );
-      sbFarClip = thisNewRibbonPanel.AddItem( sbFarClipData ) as SplitButton;
+
+      SplitButtonData sbFarClipData = new SplitButtonData( 
+        "splitFarClip", "FarClip" );
+
+      sbFarClip = thisNewRibbonPanel.AddItem( sbFarClipData ) 
+        as SplitButton;
+
       sbFarClip.AddPushButton( pbSecAdjust );
       sbFarClip.AddPushButton( pbSecAdjustOpt );
-
     }
 
     /// <summary>
@@ -76,7 +111,9 @@ namespace SplitButtonOptionConcept
     /// For the BitmapImage, make sure you reference WindowsBase
     /// and PresentationCore, and import the System.Windows.Media.Imaging namespace. 
     /// </summary>
-    BitmapImage NewBitmapImage( System.Reflection.Assembly a, string imageName )
+    BitmapImage NewBitmapImage(
+      System.Reflection.Assembly a,
+      string imageName )
     {
       Stream s = a.GetManifestResourceStream( imageName );
       BitmapImage img = new BitmapImage();
@@ -86,7 +123,9 @@ namespace SplitButtonOptionConcept
       return img;
     }
 
-    public void SetSplitButtonToThisOrTop( string _bName, SplitButton _splitButton )
+    public void SetSplitButtonToThisOrTop(
+      string _bName,
+      SplitButton _splitButton )
     {
       IList<PushButton> sbList = _splitButton.GetItems();
       foreach( PushButton pb in sbList )
@@ -105,6 +144,5 @@ namespace SplitButtonOptionConcept
       IList<PushButton> sbList = sbFarClip.GetItems();
       sbFarClip.CurrentButton = sbList[0];
     }
-
   }
 }
