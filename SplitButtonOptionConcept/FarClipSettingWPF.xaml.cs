090a1e7c41374183a3db09b322e016eec61558bf
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2016-09-06 12:08:23 +0200

    whitespace

--- SplitButtonOptionConcept/FarClipSettingWPF.xaml.cs
+++ SplitButtonOptionConcept/FarClipSettingWPF.xaml.cs
@@ -3,72 +3,86 @@ using System.Windows;
 using System.Windows.Input;
 using Autodesk.Revit.DB;
 
-namespace SplitButtonOptionConcept {
-    /// <summary>
-    /// Interaction logic for FarClipSettingWPF_B.xaml
-    /// </summary>
-    public partial class FarClipSettingWPF : Window {
-        Document _doc;
-        public FarClipSettingWPF(Document doc) {
-            InitializeComponent();
-            _doc = doc;
-            this.Top = Properties.Settings.Default.FormFarClip_Top;
-            this.Left = Properties.Settings.Default.FormFarClip_Left;
-            this.Height = Properties.Settings.Default.FormFarClip_HT;
-            this.Width = Properties.Settings.Default.FormFarClip_WD;
-            btn_Close.IsCancel = true;
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e) {
-            this.pfClip.Text = UnitFormatUtils.Format(
-                _doc.GetUnits(),
-                UnitType.UT_Length,
-                Properties.Settings.Default.PreferFarClip,
-                false,
-                false);
-        }
-
-        private void Window_Closing(object sender,
-            System.ComponentModel.CancelEventArgs e) {
-            String pfc = this.pfClip.Text;
-            try {
-                UnitType clipUnit = UnitType.UT_Length;
-                Units thisDocUnits = _doc.GetUnits();
-                double userFarClipSetting;
-                UnitFormatUtils.TryParse(thisDocUnits,
-                    clipUnit, pfc, out userFarClipSetting);
-                Properties.Settings.Default.PreferFarClip = userFarClipSetting;
-
-            } catch (Exception) {
-                MessageBox.Show("For some unknown reason.\n\nNo change was made.",
-                    "Settings Error");
-            }
-            Properties.Settings.Default.FormFarClip_Top = this.Top;
-            Properties.Settings.Default.FormFarClip_Left = this.Left;
-            Properties.Settings.Default.FormFarClip_HT = this.Height;
-            Properties.Settings.Default.FormFarClip_WD = this.Width;
-            Properties.Settings.Default.Save();
-        }
-
-        private void pfClip_KeyUp(object sender, KeyEventArgs e) {
-            if (e.Key == Key.Return) {
-                String pfc = this.pfClip.Text;
-                UnitType clipUnit = UnitType.UT_Length;
-                Units thisDocUnits = _doc.GetUnits();
-                double userFarClipSetting;
-                UnitFormatUtils.TryParse(thisDocUnits, clipUnit, pfc,
-                    out userFarClipSetting);
-                if (userFarClipSetting > 0 ) {
-                    this.pfClip.Text = UnitFormatUtils.Format(thisDocUnits,
-                        clipUnit, userFarClipSetting, false, false);
-                } else {
-                    this.pfClip.Text = "8' - 0\"";
-                }
-            }
-        }
-
-        private void btn_Close_Click(object sender, RoutedEventArgs e) {
-            this.Close();
-        }
+namespace SplitButtonOptionConcept
+{
+  /// <summary>
+  /// Interaction logic for FarClipSettingWPF_B.xaml
+  /// </summary>
+  public partial class FarClipSettingWPF : Window
+  {
+    Document _doc;
+    public FarClipSettingWPF( Document doc )
+    {
+      InitializeComponent();
+      _doc = doc;
+      this.Top = Properties.Settings.Default.FormFarClip_Top;
+      this.Left = Properties.Settings.Default.FormFarClip_Left;
+      this.Height = Properties.Settings.Default.FormFarClip_HT;
+      this.Width = Properties.Settings.Default.FormFarClip_WD;
+      btn_Close.IsCancel = true;
     }
+
+    private void Window_Loaded( object sender, RoutedEventArgs e )
+    {
+      this.pfClip.Text = UnitFormatUtils.Format(
+          _doc.GetUnits(),
+          UnitType.UT_Length,
+          Properties.Settings.Default.PreferFarClip,
+          false,
+          false );
+    }
+
+    private void Window_Closing( object sender,
+        System.ComponentModel.CancelEventArgs e )
+    {
+      String pfc = this.pfClip.Text;
+      try
+      {
+        UnitType clipUnit = UnitType.UT_Length;
+        Units thisDocUnits = _doc.GetUnits();
+        double userFarClipSetting;
+        UnitFormatUtils.TryParse( thisDocUnits,
+            clipUnit, pfc, out userFarClipSetting );
+        Properties.Settings.Default.PreferFarClip = userFarClipSetting;
+
+      }
+      catch( Exception )
+      {
+        MessageBox.Show( "For some unknown reason.\n\nNo change was made.",
+            "Settings Error" );
+      }
+      Properties.Settings.Default.FormFarClip_Top = this.Top;
+      Properties.Settings.Default.FormFarClip_Left = this.Left;
+      Properties.Settings.Default.FormFarClip_HT = this.Height;
+      Properties.Settings.Default.FormFarClip_WD = this.Width;
+      Properties.Settings.Default.Save();
+    }
+
+    private void pfClip_KeyUp( object sender, KeyEventArgs e )
+    {
+      if( e.Key == Key.Return )
+      {
+        String pfc = this.pfClip.Text;
+        UnitType clipUnit = UnitType.UT_Length;
+        Units thisDocUnits = _doc.GetUnits();
+        double userFarClipSetting;
+        UnitFormatUtils.TryParse( thisDocUnits, clipUnit, pfc,
+            out userFarClipSetting );
+        if( userFarClipSetting > 0 )
+        {
+          this.pfClip.Text = UnitFormatUtils.Format( thisDocUnits,
+              clipUnit, userFarClipSetting, false, false );
+        }
+        else
+        {
+          this.pfClip.Text = "8' - 0\"";
+        }
+      }
+    }
+
+    private void btn_Close_Click( object sender, RoutedEventArgs e )
+    {
+      this.Close();
+    }
+  }
 }
--- SplitButtonOptionConcept/Properties/AssemblyInfo.cs
+++ SplitButtonOptionConcept/Properties/AssemblyInfo.cs
@@ -5,22 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SplitButtonOptionConcept")]
+[assembly: AssemblyTitle( "SplitButtonOptionConcept" )]
 [assembly: AssemblyDescription( "C# .NET Revit add-in demonstrating use of a stacked ribbon button to access a secondary command, e.g., option settings" )]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("WTA")]
-[assembly: AssemblyProduct("Revit Add-In")]
-[assembly: AssemblyCopyright("Copyright (C) 2016 Jeremy Tammik Autodesk Inc.")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "WTA" )]
+[assembly: AssemblyProduct( "Revit Add-In" )]
+[assembly: AssemblyCopyright( "Copyright (C) 2016 Jeremy Tammik Autodesk Inc." )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible( false )]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("B0D0005F-FCDA-4AAA-9861-0926D016DD59")]
+[assembly: Guid( "B0D0005F-FCDA-4AAA-9861-0926D016DD59" )]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -38,6 +38,7 @@ using System.Runtime.InteropServices;
 // 2016-09-06 2016.0.0.0 Revit 2016 version built using Revit 2013 API assemblies and auto-installing for Revit 2015 and Revit 2016
 // 2016-09-06 2017.0.0.0 flat migration to Revit 2017 API
 // 2016-09-06 2017.0.0.1 cleanup
+// 2016-09-06 2017.0.0.2 cleanup
 //
-[assembly: AssemblyVersion("2017.0.0.1")]
-[assembly: AssemblyFileVersion("2017.0.0.1")]
+[assembly: AssemblyVersion( "2017.0.0.2" )]
+[assembly: AssemblyFileVersion( "2017.0.0.2" )]
